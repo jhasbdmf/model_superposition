5ba6ac4d3381a6724f61d7bdc536e38c0a8e0934
jhasbdmf <oshtyria@uos.de>
2025-11-22 11:16:44 +0100

    feat: simple MLP initialized

--- continual_learning.py
+++ continual_learning.py
@@ -1,6 +1,21 @@
 import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+from torch.utils.data import Subset
+import torch.nn as nn
+
+
+
+class MLP(nn.Module):
+    def __init__(self, input_dim = 28*28, hidden1=256, hidden2=256, num_classes = 10):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden1)
+        self.fc2 = nn.Linear(hidden1, hidden2)
+        self.fc_out = nn.Linear(hidden2, num_classes)
+
+    
+
+
 
 
 
@@ -11,12 +26,28 @@ train_set = datasets.MNIST(
     transform=transforms.ToTensor()
 )
 
-print (type(train_set))
-print (type(train_set[0]))
-print (len(train_set))
-print (type(train_set[0][0]))
-print (train_set[0][0].shape)
-print (type(train_set[1]))
+
+
+
+
+
+train_loader = DataLoader(train_set, batch_size=32, shuffle=False)
+
+mlp = MLP()
+
+print(mlp.fc1.weight.shape)
+
+print(mlp.fc2.weight.shape)
+print(mlp.fc_out.weight.shape)
+
+#for image, label in train_loader:
+#    flattened_image = image.reshape(image.size(0),-1)
+#    print (flattened_image.shape, label.shape)
+
+#print (len(loader))
+
+#print (loader[1])
+
 
 test_set = datasets.MNIST(
     root="./data",
@@ -31,14 +62,3 @@ print (len(test_set))
 print (type(test_set[0][0]))
 print (test_set[0][0].shape)
 print (type(test_set[1]))
-
-
-train_loader = DataLoader(train_set, batch_size=32, shuffle=False)
-
-for image, label in train_loader:
-    flattened_image = image.reshape(image.size(0),-1)
-    print (flattened_image.shape, label.shape)
-
-#print (len(loader))
-
-#print (loader[1])
