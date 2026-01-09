d0daba82740e76f75b044ce999d0423050fb49d2
jhasbdmf <oshtyria@uos.de>
2026-01-09 12:08:04 +0100

    refactor: remove old code form my experiment py

--- my_experiment.py
+++ my_experiment.py
@@ -1,85 +1,14 @@
-import torch
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-from torch.utils.data import Subset
-
-
-
-import numpy as np
 import matplotlib.pyplot as plt
 
-from utilities import MLP, train_model, run_experiment
-
-
-
-"""
-
-train_set = datasets.MNIST(
-    root="./data",
-    train=True,
-    download=True,
-    transform=transforms.ToTensor()
-)
+from utilities import run_experiment
 
 
 
 
-#print (apply_pca_to_batch(pca_instances_loader))
-
-test_set = datasets.MNIST(
-    root="./data",
-    train=False,
-    download=True,
-    transform=transforms.ToTensor()
-)
-
-
-
-
-batch_size = 128
-n_tasks = 10
-input_dim = 784
-
-
-permutations = torch.stack([torch.randperm(input_dim) for _ in range(n_tasks)])
-
-train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-
-
-
-test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)
-"""
-
-
-
-# CIFAR-10: 50k train / 10k test, 10 classes (airplane, car, ...), 32x32 RGB
-
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))  # CIFAR-10 mean/std
-])
-
-train_set = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-test_set = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-
-
-
-
-
-
-batch_size = 128
 n_tasks = 5
-input_dim = 3072
-
-train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0)
-test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=0)
-
-#print(f"Train batches: {len(train_loader)}, Classes: {train_set.classes}")
 
 
-#permutations = torch.stack([torch.randperm(input_dim) for _ in range(n_tasks)])
-#permutations = None
-
+#run with mnist, cifar, perm, diff rotation angles. with and without superposition. display all acc histories
 
 run_experiment(dataset_name = "CIFAR",
                    image_rotation_angle_per_task = 10,
