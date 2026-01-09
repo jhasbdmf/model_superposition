7290bafe4c25973016ae939b85215939e93915aa
jhasbdmf <oshtyria@uos.de>
2026-01-09 21:16:10 +0100

    test: double batch size to 2048

--- utilities.py
+++ utilities.py
@@ -256,7 +256,7 @@ def evaluate(model, loader, task_id, perm = None, device = torch.device("cuda" i
 #rather than rotated between tasks
 def run_experiment(dataset_name = "MNIST",
                    image_rotation_angle_per_task = None,
-                   batch_size = 1024,
+                   batch_size = 2048,
                    n_tasks = 5,
                    superposition = False):
 
